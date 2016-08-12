e199fefe500f91a0b49ae64ec16a666df680ec38
Alison <alisonhmullin@gmail.com>
2016-08-12 17:44:49 -0400

    build pirate lab

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,12 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(options)
+    @name = options[:name]
+    @weight = options[:weight]
+    @height = options[:height]
+    @@all << self
+  end
 end
